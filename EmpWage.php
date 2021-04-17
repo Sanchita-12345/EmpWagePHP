52cd5ca39f219379574f3a0a305eb593fc3d4b2f
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-04-17 14:24:44 +0530

    ADDED:Parttime employee and wage

--- EmpWage.php
+++ EmpWage.php
@@ -1,24 +1,40 @@
 <?php
     class EmpWage{
+        public $wage_per_hour;
+        public $full_day_hour;
+        public $part_time_hour;
+        public function __construct(){
+            $this->wage_per_hour = 20;
+            $this->full_day_hour = 8;
+            $this->part_time_hour = 4;
+        }
         public function employee_attendance(){
-            $empCheck = rand(0,1);
-            if($empCheck == 1){
-                echo "The employee is present";
+            $empCheck = rand(0,2);
+            if($empCheck == 0){
+                echo "The employee is absent";
+            }
+            else if($empCheck == 1){
+                echo "The employee is present and a full time employee.";
+                self::fulltime_employeeWage();
             }
             else{
-                echo "The employee is absent";
+                echo "The employee is present and a part time employee.";
+                self::parttime_employeeWage();
             }
         }
 
-        public function daily_employeeWage(){
-            //self::employee_attendance();
-            $wage_per_hour = 20;
-            $full_day_hour = 8;
-            //calculate daily wage of employee
-            $wage_per_day = $wage_per_hour * $full_day_hour;
-            echo "Daily wage is:".$wage_per_day;
+        public function fulltime_employeeWage(){
+            //calculate daily wage of fulltime employee
+            $wage_per_day = $this->wage_per_hour * $this->full_day_hour;
+            echo "<br>Daily wages of the full time employee is: ".$wage_per_day;
+        }
+
+        public function parttime_employeeWage(){
+            //calculate daily wage of parttime employee
+            $wage_per_day = $this->wage_per_hour * $this->part_time_hour;
+            echo "<br>Daily wages of the part time employee is: ".$wage_per_day;
         }
     }
 $obj = new EmpWage();
-$obj->daily_employeeWage();
+$obj->employee_attendance();
 ?>
