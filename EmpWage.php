28a98995e80dcfa776c5130e535ce0060d2d1f0a
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-04-20 18:53:54 +0530

    MODIFIED:Calculate daily employee wage

--- EmpWage.php
+++ EmpWage.php
@@ -1,24 +1,25 @@
 <?php
     class EmpWage{
+        public $full_day_hour;
         public function employee_attendance(){
             $empCheck = rand(0,1);
             if($empCheck == 1){
-                echo "The employee is present";
+                $this->full_day_hour = 8;
+                self::daily_employeeWage();
             }
             else{
-                echo "The employee is absent";
+                $this->full_day_hour = 0;
+                self::daily_employeeWage();
             }
         }
 
         public function daily_employeeWage(){
-            //self::employee_attendance();
             $wage_per_hour = 20;
-            $full_day_hour = 8;
             //calculate daily wage of employee
-            $wage_per_day = $wage_per_hour * $full_day_hour;
+            $wage_per_day = $wage_per_hour * $this->full_day_hour;
             echo "Daily wage is:".$wage_per_day;
         }
     }
 $obj = new EmpWage();
-$obj->daily_employeeWage();
+$obj->employee_attendance();
 ?>
