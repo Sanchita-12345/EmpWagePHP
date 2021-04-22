6b7235d58b6b452f9c168c62ada53b9c5d737694
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-04-22 13:32:19 +0530

    MODIFIED:Total wage for each company

--- EmpWage.php
+++ EmpWage.php
@@ -1,59 +1,49 @@
 <?php
     class EmpWage{
-        public $full_day_hour = 8;
-        public $part_time_hour = 4;
-        public $wage_per_hour = 20;
-        public $max_working_day_per_month;
-        public $max_working_hour_per_month;
+        public $IS_PART_TIME = 1;
+        public $IS_FULL_TIME =2;
+        public $wage_per_hour =20;
+        public $num_of_working_days = 20;
+        public $max_working_hour_per_month = 100;
         public $companyName;
-        public $wage_per_month;
-        
-        public function __construct($companyName, $wage_per_month, $max_working_day_per_month, $max_working_hour_per_month){
+
+        public function __construct($companyName, $wage_per_day, $num_of_working_days, $max_working_hour_per_month){
             $this->companyName = $companyName;
-            $this->wage_per_month = $wage_per_month;
-            $this->max_working_day_per_month = $max_working_day_per_month;
+            $this->wage_per_day = $wage_per_day;
+            $this->num_of_working_days = $num_of_working_days;
             $this->max_working_hour_per_month = $max_working_hour_per_month;
         }
-        public function employee_attendance(){
-            $empCheck = rand(1,2);
-            switch($empCheck){
-                // case 0:
-                //     echo "The employee is absent";
-                //     break;
-                case 1 :
-                    echo " and he employee is present and a full time employee.";
-                    self::fulltime_employeeWage();
-                    break;
-                case 2:
-                    echo " and the employee is present and a part time employee.";
-                    self::parttime_employeeWage();
-                    break;
+        function computeEmployeeWage(){
+            $totalEmpWage = 0;
+            $totalEmpHrs = 0;
+            $totalWorkingDays = 0;
+            $empHrs = 0;
+            while ($totalEmpHrs <= $this->max_working_hour_per_month &&
+                $totalWorkingDays < $this->num_of_working_days) {
+                $totalWorkingDays++;
+                $empCheck = rand(1,2);
+                switch($empCheck){
+                    case 1:
+                        $empHrs = 4;
+                        break;
+                    case 2:
+                        $empHrs = 8;
+                        break;
+                    default:
+                        $empHrs = 0;
+                        break;
+                }
+                $totalEmpHrs += $empHrs;
+                echo "<br>Days : ".$totalWorkingDays." <br>Hours : ".$empHrs;
             }
-        }
-
-        public function fulltime_employeeWage(){
-            //calculate daily wage of fulltime employee
-            if($this->max_working_hour_per_month >= 100 || $this->max_working_day_per_month >= 20){
-                $wage_per_month = $this->max_working_hour_per_month * $this->max_working_day_per_month * $this->wage_per_hour * $this->full_day_hour;
-            echo "<br>Monthly wage of this employee is : ".$wage_per_month;
-            }
-        }
-
-        public function parttime_employeeWage(){
-            //calculate daily wage of parttime employee
-            if($this->max_working_hour_per_month >= 100 || $this->max_working_day_per_month >= 20){
-                $wage_per_month = $this->max_working_hour_per_month * $this->max_working_day_per_month * $this->wage_per_hour * $this->part_time_hour;
-            echo "<br>Monthly wage of this employee is : ".$wage_per_month;
-            }
+            $totalEmpWage = $totalEmpHrs * $this->wage_per_hour;
+            return $totalEmpWage;
         }
     }
-$obj1 = new EmpWage("Dmart", 100, 26, 30);
-//echo "<br>The company is ".$obj1->companyName;
-$computeWageArray = array($companyName, $wage_per_month, $max_working_day_per_month, $max_working_hour_per_month);
-print_r($computeWageArray);
-$obj1->employee_attendance();
-
-// $obj2 = new EmpWage("Bigbasket", 150, 20, 40);
-// echo "<br>The company is ".$obj2->companyName;
-// $obj2->employee_attendance();
+    $obj1 = new EmpWage("Dmart", 100, 26, 30);
+    $obj2 = new EmpWage("Bigbasket", 150, 20, 40);
+    echo "<br>The company is ".$obj1->companyName;
+    echo "<br>Total Employee wage for this company is : ".$obj1->computeEmployeeWage();
+    echo "<br><br>The company is ".$obj2->companyName;
+    echo "<br>Total Employee wage for this company is : ".$obj2->computeEmployeeWage();
 ?>
