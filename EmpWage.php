9696fc6054d5586568f264df83fef1d4dc39e289
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-04-26 10:27:30 +0530

    MODIFIED:Check the employee is present or absent

--- EmpWage.php
+++ EmpWage.php
@@ -1,15 +1,10 @@
 <?php
-    class EmpWage{
-        public function employee_attendance(){
-            $empCheck = rand(0,1);
-            if($empCheck == 1){
-                echo "The employee is present";
-            }
-            else{
-                echo "The employee is absent";
-            }
-        }
+    $IS_FULL_TIME = 1;
+    $empCheck = rand(0,1);
+    if($empCheck == $IS_FULL_TIME){
+        echo "The employee is present";
     }
-$obj = new EmpWage();
-$obj->employee_attendance();
+    else{
+        echo "The employee is absent";
+    }
 ?>
